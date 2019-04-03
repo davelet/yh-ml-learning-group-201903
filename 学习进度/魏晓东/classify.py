77e812e7492b22a7e169aa0d89a722552f8cc45b
魏晓东 <358537858@qq.com>
2019-04-03 13:14:47 +0800

    0404

---
+++ 学习进度/魏晓东/classify.py
@@ -0,0 +1,17 @@
+# TensorFlow and tf.keras
+import tensorflow as tf
+from tensorflow import keras
+
+# Helper libraries
+import numpy as np
+import matplotlib.pyplot as plt
+
+print(tf.__version__)
+
+fashion_mnist = keras.datasets.fashion_mnist
+(train_images, train_lables),(test_images, test_labels) = fashion_mnist.load_data()
+
+class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat',
+               'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
+            
+print(train_images.shape)
